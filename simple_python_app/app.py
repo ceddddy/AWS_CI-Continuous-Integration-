1a9f333d66c5a14740db1af19845691412abb5f2
Drich <drich@Drichs-MacBook-Pro.local>
2025-01-03 12:12:00 +0200

    testing CI pipeline

--- simple_python_app/app.py
+++ simple_python_app/app.py
@@ -4,7 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world!, This is a milestone numer one to be specific.'
+    return 'Hello, world!, This is a milestone numer one to be specific. One, two, one, two - this is a test for the CI/CD pipeline, focusing on the CI part.'
 
 if __name__ == '__main__':
     app.run()
+
+# The above is a simple python app that uses the Flask framework to create a web server that listens on port 5000 and returns a simple message when you visit the root URL.
